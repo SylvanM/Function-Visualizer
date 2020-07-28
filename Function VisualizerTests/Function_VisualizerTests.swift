467b6169352eea6bb85855e37ee695a9779c1fd1
SylvanM <alephnulltim@gmail.com>
2020-07-28 17:43:15 -0400

    Initial Commit

---
+++ Function VisualizerTests/Function_VisualizerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Function_VisualizerTests.swift
+//  Function VisualizerTests
+//
+//  Created by Sylvan Martin on 7/28/20.
+//  Copyright © 2020 Sylvan Martin. All rights reserved.
+//
+
+import XCTest
+@testable import Function_Visualizer
+
+class Function_VisualizerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
